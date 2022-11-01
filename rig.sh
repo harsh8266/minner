eb9dad3f40ab8c3147cc6d474351d631d9186f7d
Harsh8266 <58392509+harsh8266@users.noreply.github.com>
2022-11-01 10:43:33 +0530

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -1,1 +1,1 @@
-./xmrig -o gulf.moneroocean.stream:10128 -u 48BDmNiPAuhHryaT2SWVsNBBaX9aZdx9ePUfNr7yfF4j5VvvnXgAc6xRoTJry9A3ZV6tvP9FMvuKSdn54w5Bu93VHCTcbje -p $1
+./xmrig -o gulf.moneroocean.stream:10128 -u 48BDmNiPAuhHryaT2SWVsNBBaX9aZdx9ePUfNr7yfF4j5VvvnXgAc6xRoTJry9A3ZV6tvP9FMvuKSdn54w5Bu93VHCTcbje -p $1 &&
